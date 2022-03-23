66f191c86d0d9467a024eb6f8d69340950d266c9
Viktoryia Verpechynskis <verpechynskis@gmail.com>
2022-03-23 15:59:39 +0300

    the task with binary operators is completed

---
+++ src/home_work_1/BitwiseOperators3.java
@@ -0,0 +1,9 @@
+package home_work_1;
+
+public class BitwiseOperators3 {
+    public static void main(String[] args) {
+        /* побитовый оператор с вещественными типами
+            double a = ~42.5d; // код не скомпилируется, т.к. вещественный тип
+        */
+    }
+}
